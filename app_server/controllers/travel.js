4dcd777e2eeb9bb18b38c97738b466865fe5db9d
jmdonnell423 <jmdonnell0501@gmail.com>
2024-11-17 10:36:30 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,21 @@
-/*GET Travel View*/
+const fs = require('fs');
+const path = require('path');
+
+let trips = [];
+try {
+    trips = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/trips.json'), 'utf8'));
+} catch (error) {
+    console.error('Error reading trips.json:', error);
+}
+
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
+    res.render('travel', {
+        title: 'Travlr Getaways',
+        trips
+    });
 };
 
 module.exports = {
     travel
 };
+
