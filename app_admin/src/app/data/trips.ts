f913daff7914ab39adb94efd0c9e0fbd0e9e1758
jmdonnell423 <jmdonnell0501@gmail.com>
2024-12-08 11:49:05 -0500

    Module 6 in progress

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,32 @@
+export const trips = [
+    {
+        "code":  "GALR210214",
+        "name":  "Gale Reef",
+        "length": "4 nights / 5 days",
+        "start":  "2021-02-14T08:00:00Z",
+        "resort":  "Emerald Bay, 3 stars",
+        "perPerson":  "799.00",
+        "image": "reef1.jpg",
+        "description": "<p>Gale Reef - Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis.</p>" 
+    },
+    {
+        "name": "Dawson's Reef",
+        "image": "reef2.jpg",
+        "description": "<p>Dawson's Reef - Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis.</p>",
+        "code": "DAWR210315",
+        "length": "4 nights / 5 days",
+        "start":  "2021-03-15T08:00:00Z",
+        "resort": "Blue Lagoon, 4 stars",
+        "perPerson": "1199.00"
+    },
+    {
+        "name": "Claire's Reef",
+        "image": "reef3.jpg",
+        "description": "<p>Claire's Reef - Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis.</p>",
+        "code": "CLAR210315",
+        "length": "4 nights / 5 days",
+        "start":  "2021-06-21T08:00:00Z",
+        "resort": "Blue Lagoon, 4 stars",
+        "perPerson": "1999.00"
+    }
+];
