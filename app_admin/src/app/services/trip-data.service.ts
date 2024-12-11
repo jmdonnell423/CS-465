b27f259ce50c1d9c98bd8345cf8bd4ff6d05ac75
jmdonnell423 <jmdonnell0501@gmail.com>
2024-12-10 23:15:26 -0500

    Module 6 in progress

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -10,10 +10,14 @@ import { Trip } from '../models/trip';
 export class TripDataService {
 
   constructor(private http: HttpClient) { }
+  url = 'http://localhost:3000/api/trips';
 
   getTrips() : Observable<Trip[]> {
-    let url = 'http://localhost:3000/api/trips';
-
-    return this.http.get<Trip[]>(url);
+    return this.http.get<Trip[]>(this.url);
   }
+
+  addTrips(formData: Trip) : Observable<Trip> {
+    return this.http.post<Trip>(this.url, formData);
+  }
+
 }
